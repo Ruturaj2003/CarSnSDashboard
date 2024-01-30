07c4cdbdc453f6d4923a208e5c6c34b5fae723e2
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-30 20:32:06 +0530

    Navbar Customer Proto 3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,39 @@
 const Navbar = () => {
   return (
     <>
-      <div className="bg-slate-500 h-16 mt-6 mr-16 ml-16 flex items-center justify-between ">
+      <div className="h-16 mt-6 mr-16 ml-16 flex items-center justify-between ">
         <div
           className="
-        bg-slate-800 w-[55%] h-full   
+      w-[55%] h-full   
         flex items-center
-        justify-around
+        
         "
         >
+          {' '}
           {/* Logo */}
-          <div className="bg-white rounded-full h-[50px] w-[50px]"></div>
+          <div className=" ml-4 mr-8 bg-blue-200 rounded-full h-[50px] w-[50px]"></div>
           {/* Navlink Container */}
-          <div className="bg-slate-300 h-full w-[70%] flex items-center justify-evenly">
-            <div className="h-full bg-slate-200 border-b-2   transition-all duration-500 hover:border-blue-600 flex justify-center items-center ">
+          <div className="300 h-full w-[70%] flex items-center justify-evenly">
+            <div className="h-full 2 border-b-2   transition-all duration-500 hover:border-blue-600 flex justify-center items-center ">
               <h1>Losadae</h1>
             </div>
-            <div className="h-full bg-slate-200 border-b-2   transition-all duration-500 hover:border-blue-600 flex justify-center items-center ">
+            <div className="h-full  border-b-2   transition-all duration-500 hover:border-blue-600 flex justify-center items-center ">
               <h1>Losadae</h1>
             </div>
-            <div className="h-full bg-slate-200 border-b-2   transition-all duration-500 hover:border-blue-600 flex justify-center items-center ">
+            <div className="h-full  border-b-2   transition-all duration-500 hover:border-blue-600 flex justify-center items-center ">
               <h1>Losadae</h1>
             </div>
-            <div className="h-full bg-slate-200 border-b-2   transition-all duration-500 hover:border-blue-600 flex justify-center items-center ">
+            <div className="h-full border-b-2   transition-all duration-500 hover:border-blue-600 flex justify-center items-center ">
               <h1>Losadae</h1>
             </div>
-            <div className="h-full bg-slate-200 border-b-2   transition-all duration-500 hover:border-blue-600 flex justify-center items-center ">
+            <div className="h-full 200 border-b-2   transition-all duration-500 hover:border-blue-600 flex justify-center items-center ">
               <h1>Losadae</h1>
             </div>
           </div>
         </div>
-        <div className="bg-slate-800 w-[200px] h-full"></div>
+        <div className="bg-slate-200 w-[200px] h-full"></div>
       </div>
-      <div className="bg-slate-950 w-full h-[1px]"></div>
+      <div className="bg-slate-950 mr-16 ml-16 w-full h-[1px]"></div>
     </>
   );
 };
