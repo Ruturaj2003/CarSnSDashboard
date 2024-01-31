2589613f566ebbbcc9b94f3fb4cfc9dab4a5a1d6
Wazid <munavalliwazid@gmail.com>
2024-01-31 15:54:24 +0530

    Add Navbar Active Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Sidebar></Sidebar>
     </>
   );
 }
