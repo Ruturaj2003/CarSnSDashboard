b4039a06c096ee5cdd3d105804c842929a8f1983
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-02-01 11:32:25 +0530

    Create Card Component

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+  return (
+    <div className="flex  rounded-lg shadow-md flex-col w-[300px] h-[200px]   p-6 items-center justify-center">
+      <div className="h-[60%] w-full">43</div>
+      <div className="h-[30%] text-4xl text-center  w-full">asd</div>
+    </div>
+  );
+};
+export default Card;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import Card from '../components/Card';
+
 const Home = () => {
   return (
     <div className="w-full h-600px mt-5 flex flex-col mr-16">
@@ -11,21 +13,19 @@ const Home = () => {
       {/* Cards Container */}
       <div className="flex flex-col h-[540px] w-full mr-16 bg-red-50 ">
         {/* Card Col 1 */}
-        <div className="w-full h-[270px]  bg-slate-300 p-5 flex items-center justify-evenly ">
+        <div className="w-full h-[270px]  bg-slate-50 p-5 flex items-center justify-evenly ">
           {/* Card 1  */}
-          <div className="flex flex-col w-[300px] h-[200px] bg-green-100  p-6 items-center justify-center">
-            <div className="h-[60%] w-full bg-slate-400">43</div>
-            <div className="h-[30%] text-4xl text-center bg-slate-200 w-full">
-              asd
-            </div>
-          </div>
+          <Card></Card>
           {/* Card 2 */}
-          <div className="flex flex-col w-[300px] h-[200px] bg-green-100  p-6 items-center justify-center">
-            <div className="h-[60%] w-full bg-slate-400">43</div>
-            <div className="h-[30%] text-4xl text-center bg-slate-200 w-full">
-              asd
-            </div>
-          </div>
+          <Card></Card>
+        </div>
+
+        {/* Card Col 2 */}
+        <div className="w-full h-[270px]  bg-slate-50 p-5 flex items-center justify-evenly ">
+          {/* Card 3  */}
+          <Card></Card>
+          {/* Card 4 */}
+          <Card></Card>
         </div>
       </div>
     </div>
