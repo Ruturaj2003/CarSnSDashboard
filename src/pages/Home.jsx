6268689591fc07841f597b9e25e2784902edee29
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-02-01 12:00:35 +0530

    Update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import Card from '../components/Card';
 import { MdOutlinePendingActions } from 'react-icons/md';
+import { IoMdDoneAll } from 'react-icons/io';
+import { FaPeopleGroup } from 'react-icons/fa6';
+import { FaMoneyBillTrendUp } from 'react-icons/fa6';
 const Home = () => {
   return (
     <div className="w-full h-600px mt-5 flex flex-col mr-16">
       {/* Heading Container */}
       <div className="flex flex-col ">
-        <h1 className=" text-center [word-spacing:25px] tracking-wider  text-5xl font-homeT">
+        <h1 className=" text-center [word-spacing:25px] tracking-wider text-[#797979]  text-5xl font-homeT">
           Business Operations Analytics{' '}
         </h1>
         <div className="border-b-2 border-b-slate-200"></div>
@@ -19,15 +22,21 @@ const Home = () => {
             <MdOutlinePendingActions className="text-8xl text-[#0066AD] mx-auto" />
           </Card>
           {/* Card 2 */}
-          <Card></Card>
+          <Card desc={'Serviced'} number={69}>
+            <IoMdDoneAll className="text-8xl text-[#0066AD] mx-auto" />
+          </Card>
         </div>
 
         {/* Card Col 2 */}
         <div className="w-full h-[270px]  bg-slate-50 p-5 flex items-center justify-evenly ">
           {/* Card 3  */}
-          <Card></Card>
+          <Card desc={'Employees'} number={420}>
+            <FaPeopleGroup className="text-8xl text-[#0066AD] mx-auto" />
+          </Card>
           {/* Card 4 */}
-          <Card></Card>
+          <Card desc={'Turn Over'} number={'12M'}>
+            <FaMoneyBillTrendUp className="text-8xl text-[#0066AD] mx-auto" />
+          </Card>
         </div>
       </div>
     </div>
