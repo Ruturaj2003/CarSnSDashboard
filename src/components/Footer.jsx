99b9227a9694f705d12183410648b2cfcb3563d3
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-02-02 15:16:08 +0530

    Add Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,26 @@
+import { FaGithub } from 'react-icons/fa';
+import { FaMeta, FaXTwitter } from 'react-icons/fa6';
 const Footer = () => {
-  return <div className="w-full h-[100px] bg-slate-600">Footer</div>;
+  return (
+    <footer className="w-full bg-slate-900 text-white">
+      <div className="container mx-auto py-8 flex flex-col items-center justify-center">
+        <p className="text-center text-lg">
+          © 2024 Dotaizz. All Rights Reserved.
+        </p>
+
+        <div className="mt-4 flex space-x-4">
+          <a href="#" className="text-xl">
+            <FaXTwitter className="hover:text-[#2c95e0] hover:scale-150" />
+          </a>
+          <a href="#" className="text-xl">
+            <FaMeta className="hover:text-[#2c95e0] hover:scale-150" />
+          </a>
+          <a href="#" className="text-xl">
+            <FaGithub className="hover:text-[#2c95e0] hover:scale-150" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
