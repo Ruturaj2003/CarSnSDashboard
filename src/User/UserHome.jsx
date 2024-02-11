11517069518f918841e6559c0257cf32f8967cf3
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-02-12 00:16:10 +0530

    Add Carousel Components

--- src/User/UserHome.jsx
+++ src/User/UserHome.jsx
@@ -1,9 +1,21 @@
-import hero from './assets/hero.jpg';
+import { CCarousel } from '@coreui/react';
 import Hero1 from './components/Hero/Hero1';
 const UserHome = () => {
   return (
     <div className=" relative">
-      <Hero1></Hero1>
+      {/* <Hero1></Hero1> */}
+      <CCarousel controls transition="crossfade">
+        <Hero1></Hero1>
+        {/* <CCarouselItem>
+          <CImage className="d-block w-100" src={ReactImg} alt="slide 1" />
+        </CCarouselItem>
+        <CCarouselItem>
+          <CImage className="d-block w-100" src={VueImg} alt="slide 2" />
+        </CCarouselItem>
+        <CCarouselItem>
+          <CImage className="d-block w-100" src={AngularImg} alt="slide 3" />
+        </CCarouselItem> */}
+      </CCarousel>
     </div>
   );
 };
