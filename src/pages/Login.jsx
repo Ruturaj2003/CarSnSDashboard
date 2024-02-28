982df19887a0494439e09467e696fb045d279f62
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-02-28 12:28:11 +0530

    Fix Navigation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -12,112 +11,118 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { Grid } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
 
 const defaultTheme = createTheme();
 
-function Alogin() {
+function AdminLogin() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
 
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
     });
+  };
 
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value,
-        });
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    try {
+      const response = await axios.post(
+        'http://localhost:8081/admin',
+        formData
+      );
+      console.log('Login successful', response.data);
+      toast.success('Login successful');
+      navigate('home');
+    } catch (error) {
+      console.error('Login failed', error);
+      toast.error('Login failed');
+    }
+  };
 
-        try {
-            const response = await axios.post('http://localhost:8081/admin', formData);
-            console.log('Login successful', response.data);
-            toast.success('Login successful');
-            navigate('home')
-
-        } 
-        catch (error) {
-            console.error('Login failed', error);
-            toast.error('Login failed');
-        }
-    };
-
-    return (
-        <>
-            <ToastContainer
-                position="top-center"
-                autoClose={2000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
-            <ThemeProvider theme={defaultTheme}>
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline />
-                    <Box
-                        sx={{
-                            marginTop: 8,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                            <PersonIcon />
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
-                            Admin Log in
-                        </Typography>
-                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                            <TextField
-                                fullWidth
-                                id="email"
-                                label="Username"
-                                name="email"
-                                autoComplete="email"
-                                onChange={handleChange}
-                            />
-                            <TextField
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                                sx={{ mt: 2 }}
-                                onChange={handleChange}
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Log In
-                            </Button>
-                            <Grid container justifyContent="flex-end">
-                                <Grid item>
-                                    <ReactRouterDom.Link to="ulogin" variant="body2">
-                                        User login
-                                    </ReactRouterDom.Link>
-                                </Grid>
-                            </Grid>
-                        </Box>
-                    </Box>
-                </Container>
-            </ThemeProvider>
-        </>
-    );
+  return (
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <PersonIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Admin Log in
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 3 }}
+            >
+              <TextField
+                fullWidth
+                id="email"
+                label="Username"
+                name="email"
+                autoComplete="email"
+                onChange={handleChange}
+              />
+              <TextField
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                sx={{ mt: 2 }}
+                onChange={handleChange}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Log In
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link to="/ulogin" variant="body2">
+                    User login
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default Alogin;
+export default AdminLogin;
