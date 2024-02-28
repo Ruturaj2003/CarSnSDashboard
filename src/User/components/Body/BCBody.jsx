c6af53c52f94a65334155e34f7e888fa6e29610d
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-02-28 20:48:50 +0530

    Create Body COmponent for BRC

---
+++ src/User/components/Body/BCBody.jsx
@@ -0,0 +1,16 @@
+const BCBody = () => {
+  return (
+    <>
+      <div className="flex justify-center flex-col items-center">
+        {/* Heading */}
+        <div className="flex justify-center items-center flex-col">
+          <h3 className="text-6xl w-full"> Browse Cars</h3>
+          <div className="w-full border-b-4 border-b-[#0066B1]"></div>
+        </div>
+
+        {/* Content */}
+      </div>
+    </>
+  );
+};
+export default BCBody;
--- src/User/pages/BrowseCar.jsx
+++ src/User/pages/BrowseCar.jsx
@@ -3,12 +3,15 @@ import hero from '../assets/browCar.jpg';
 import { FaQuoteLeft } from 'react-icons/fa';
 import { FaQuoteRight } from 'react-icons/fa';
 import BCHero from '../components/Hero/BCHero';
+import BCBody from '../components/Body/BCBody';
 const BrowseCar = () => {
   return (
     <>
       <UserNavbar></UserNavbar>
       {/* Hero */}
       <BCHero></BCHero>
+      {/* Body */}
+      <BCBody></BCBody>
     </>
   );
 };
