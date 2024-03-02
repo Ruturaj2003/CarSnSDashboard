97649a69003059756360b59354c87a43134d5996
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-03-02 21:12:33 +0530

    MInor Update

--- src/User/components/Body/BCCard.jsx
+++ src/User/components/Body/BCCard.jsx
@@ -19,14 +19,14 @@ const BCCard = ({ data }) => {
       {/* Image COntainer */}
       <div className="h-[80%] w-full bg-blue-500 rounded-md mb-3">
         <img
-          src="https://imgs.search.brave.com/xUUC0xEbQYZ2b_PJFHW8KmqRIoPAFJvwKCbOBLRJjCY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvZmVhdHVy/ZWQvYm13LWQ5NDJh/M3p4ZDhpM3VxYzgu/anBn"
+          src={`http://localhost:8081/images/${data.carimage}`}
           alt=""
           className="h-full w-full object-cover"
         />
       </div>
       {/* Name of Image */}
       <div className="h-[10%]">
-        <h3 className="text-3xl font-tableD text-center">BMW X7</h3>
+        <h3 className="text-3xl font-tableD text-center">{data.modelname}</h3>
       </div>
     </div>
   );
